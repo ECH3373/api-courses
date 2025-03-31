4e2a919f90257914ad693020c1adb30fe43ed300
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-31 13:14:26 -0600

    Update lesson query

--- src/features/lesson/query.js
+++ src/features/lesson/query.js
@@ -2,8 +2,8 @@ import { helpers } from '../../helpers/index.js';
 
 const index = helpers.query.index({
   search: ['name', 'duration_seconds'],
-  filters: ['name', 'duration_seconds', 'course_id'],
-  sort: ['name', 'duration_seconds', 'course_id', 'order'],
+  filters: ['name', 'duration_seconds', 'module_id'],
+  sort: ['name', 'duration_seconds', 'module_id', 'order'],
 });
 
 export const query = {
